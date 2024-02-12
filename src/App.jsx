95c794e83a713428927b1ef302105fb1a7e6797c
JoserynE <joseryncaraballo@hotmail.com>
2024-02-12 20:31:15 -0300

    mi primera app react ejercicios-mesa

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import Navbar from "./Navbar.jsx";
+import Home from "./Home.jsx";
+import Footer from "./Footer.jsx";
 
 function App() {
-  return <div>
-    
-  </div>;
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
